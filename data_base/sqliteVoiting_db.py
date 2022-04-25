e145c5db6083d0601192a5f0bfd8a6bbfab15c91
Alexey Lisitsyn <llisicyn@bk.ru>
2022-04-25 19:01:45 +0300

    finaltest8

--- data_base/sqliteVoiting_db.py
+++ data_base/sqliteVoiting_db.py
@@ -3,25 +3,25 @@ from create_bot import bot
 import os
 
 def sqk_start():
-    global base, cur
-    base = ps.connect(os.environ.get('DATABASE_URL'), sslmode='require')
-    cur = base.cursor()
-    if base:
+    global base2, cur2
+    base2 = ps.connect(os.environ.get('DATABASE_URL'), sslmode='require')
+    cur2 = base2.cursor()
+    if base2:
         print('Data base for voiting connected OK')
 
 
 
 async def sql_add_command(state):
     async with state.proxy() as data:
-        cur.execute('INSERT INTO ans VALUES (%s)', tuple(data.values()))
-        base.commit()
+        cur2.execute('INSERT INTO ans VALUES (%s)', tuple(data.values()))
+        base2.commit()
 
 
 async def sql_read(message):
     likes = 0
     dislikes = 0
-    cur.execute('SELECT * FROM ans')
-    for ret in cur.fetchall():
+    cur2.execute('SELECT * FROM ans')
+    for ret in cur2.fetchall():
         if ret[0] == '1':
             likes += 1
         else:
